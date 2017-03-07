844c482e8f8d58ac9c4395b8d8d0d05e70778d75
Madhav Ghei <madhav.ghei@gmail.com>
2017-03-07 14:54:18 -0600

    added asian cuisine transformation

--- transformation.py
+++ transformation.py
@@ -1,12 +1,66 @@
 
-ing_list = ['chicken', 'salt', 'bread crumbs']
+ing_list = ['chicken', 'salt', 'bread crumbs', 'oil']
 h = {'salt':'garlic powder'}
 v = {'chicken':'seitan'}
 gf = {'bread crumbs':'ground flaxseeds'}
+asian = {'wine':'rice wine',
+    'cooking oil':'sesame oil', 
+    'oil':'sesame oil', 
+    'mozzerella':'rushan',
+    'mozzerella cheese':'rushan',
+    'cheddar cheese' : 'rushan',
+    'cheddar' : 'rushan',
+    'blue cheese': 'rushan',
+    'parmesan': 'rushan',
+    'parmesan cheese': 'rushan',
+    'romano' : 'rushan',
+    'romano cheese' : 'rushan',
+    'gouda' : 'rushan',
+    'gouda cheese' : 'rushan',
+    'swiss': 'rushan',
+    'swiss cheese': 'rushan',
+    'cheddar' : 'rushan',
+    'cheddar cheese' : 'rushan',
+    'provolone' : 'rushan',
+    'provolone cheese' : 'rushan',
+    'feta': 'rushan',
+    'feta cheese': 'rushan',
+    'cream cheese' : 'rubing',
+    'yogurt' : 'nai lao',
+    'gravy' : 'chinese brown gravy',
+    'sausage':'chicken',
+    'anchovies' : 'shrimp',
+    'bacon' : 'salted pork',
+    'okra' : 'bok choy',
+    'asparagus' : 'bok choy',
+    'squash' : 'choy sum',
+    'corn' : 'baby corn',
+    'refried beans' : 'green beans',
+    'chick peas' : 'peking chick peas',
+    'butternut squash' : 'oriental squash',
+    'lettuce' : 'bok choy',
+    'broccoli' : 'kai-lan',
+    'blueberries' : 'chinese blueberries',
+    'watermelon' : 'wax gourd',
+    'arugula' : 'chinese cabbage',
+    'olives' : 'chinese olives',
+    'spaghetti squash' : 'string beans',
+    'penne pasta' : 'noodles',
+    'spaghetti' :'noodles',
+    'macaroni': 'rice',
+    'elbow pasta' : 'rice',
+    'pasta' :'rice',
+    'olive oil': 'peanut oil',
+    'jalapeno' : 'red or green chili',
+    'tobasco': 'sriracha',
+    'hot sauce': 'sriracha',
+    'habanero pepper' : 'red or green chili',
+    }
+
 
 
 def recipe_transform(ingredients):
-    transformation = raw_input('Choose a transformation (h = healthy, v = vegetarian, gf = gluten free): ')
+    transformation = raw_input('Choose a transformation (h = healthy, v = vegetarian, gf = gluten free, asian = Asian): ')
 
     if transformation == 'h':
         count = 0
@@ -26,6 +80,13 @@ def recipe_transform(ingredients):
         count = 0
         for i in ingredients:
             new_ing = gf.get(i,i)
+            ingredients[count] = new_ing
+            count += 1
+        print ingredients
+    elif transformation == 'asian':
+        count = 0
+        for i in ingredients:
+            new_ing = asian.get(i,i)
             ingredients[count] = new_ing
             count += 1
         print ingredients
