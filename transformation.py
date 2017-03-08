af2fc2b94a1745a7e8aaa39d70956e9e0931822b
Madhav Ghei <madhav.ghei@gmail.com>
2017-03-07 20:15:20 -0600

    added italian and indian cuisine transformations

--- transformation.py
+++ transformation.py
@@ -1,5 +1,5 @@
 
-ing_list = ['chicken', 'salt', 'bread crumbs', 'oil']
+ing_list = ['beef', 'salt', 'pepper', 'oil', 'jalapeno']
 h = {'salt':'garlic powder'}
 v = {'chicken':'seitan'}
 gf = {'bread crumbs':'ground flaxseeds'}
@@ -57,10 +57,147 @@ asian = {'wine':'rice wine',
     'habanero pepper' : 'red or green chili',
     }
 
+italian = {
+    'jalapeno' : 'chili flakes',
+    'wasabi': 'chili flakes',
+    'tobasco': 'chili flakes',
+    'hot sauce': 'chili flakes',
+    'habanero pepper' : 'chili flakes',
+    'cheddar' : 'gorgonzola',
+    'anejo enchilado' :'gorgonzola',
+    'blue': 'robiola piemonte',
+    'cotija' : 'mozzarella',
+    'gruyere' : 'pecorino toscano',
+    'romano' : 'pecorino toscano',
+    'gouda' : 'robiola piemonte',
+    'raclette': 'asiago',
+    'swiss': 'asiago',
+    'cheddar' : 'mozzarella',
+    'chevres' : 'robiola piemonte',
+    'edam' : 'parmigiano-reggiano',
+    'monterey jack': 'parmigiano-reggiano',
+    'butterkase' : 'mozzarella',
+    'colby': 'asiago',
+    'havarti': 'mozzarella',
+    'brie' : 'gorgonzola',
+    'camembert de normandie' :'robiola piemonte',
+    'limburger' : 'asiago',
+    'feta': 'pecorino toscano',
+    'munster' : 'gorgonzola',
+    'neufchatel' : 'ricotta',
+    'queso' : 'mozzarella',
+    'curd' : 'burrata',
+    'chechil' : 'gorgonzola',
+    'chura kampo' : 'ricotta',
+    'american cheese' : 'mozzarella',
+    'roquefort': 'piave',
+    'camembert': 'mozzarella',
+    'cotija': 'asiago',
+    'chevre': 'parmigiano-reggiano',
+    'emmental': 'asiago',
+    'manchego': 'piave',
+    'cream cheese' : 'mascarpone',
+    'yogurt' : 'italian yogurt',
+    'gravy' : 'italian gravy',
+    'squash' : 'zucca',
+    'refried bean' : 'fava bean',
+    'lima bean': 'fava bean',
+    'butternut squash' : 'zucca',
+    'lettuce' : 'radicchio',
+    'olives' : 'castelvetrano olives',
+    'celery' : 'tomato',
+    'spaghetti squash' : 'zucca',
+    'noodles' : 'spaghetti',
+    'rice': 'pasta',
+    'peanut oil': 'olive oil',
+    'wok': 'pan',
+    'pork': 'chicken',
+    'bacon': 'guanciale'
+}
+
+indian = {
+    'jalapeno' : 'green chili',
+    'wasabi': 'chili paste',
+    'tobasco': 'chili paste',
+    'hot sauce': 'chili paste',
+    'habanero pepper' : 'red chili',
+    'cheese':'paneer',
+    'mozzerella': 'paneer',
+    'cheddar' : 'paneer',
+    'anejo enchilado' :'paneer',
+    'asiago':'paneer',
+    'blue': 'paneer',
+    'parmesan': 'paneer',
+    'romano' : 'paneer',
+    'gouda' : 'paneer',
+    'swiss': 'paneer',
+    'cheddar' : 'paneer',
+    'chevres' : 'paneer',
+    'edam' : 'paneer',
+    'monterey jack': 'paneer',
+    'provolone' : 'paneer',
+    'butterkase' : 'paneer',
+    'colby': 'paneer',
+    'fontina val daosta':'paneer',
+    'havarti': 'paneer',
+    'brie' : 'paneer',
+    'camembert de normandie' :'paneer',
+    'gorgonzola' : 'paneer',
+    'limburger' : 'paneer',
+    'feta': 'paneer',
+    'munster' : 'paneer',
+    'neufchatel' : 'paneer',
+    'queso' : 'paneer',
+    'burrata' : 'paneer',
+    'curd' : 'paneer',
+    'chechil' : 'paneer',
+    'chura kampo' : 'paneer',
+    'american cheese' : 'paneer',
+    'roquefort': 'paneer',
+    'camembert': 'paneer',
+    'emmental': 'paneer',
+    'taleggio': 'paneer',
+    'parmigiano-reggiano': 'paneer',
+    'manchego': 'paneer',
+    'cream cheese' : 'yogurt',
+    'yogurt' : 'dahi',
+    'tofu' : 'paneer',
+    'beef': 'mutton',
+    'okra' : 'bhindi',
+    'asparagus' : 'bhindi',
+    'squash' : 'squash',
+    'refried beans' : 'rajma',
+    'chick peas' : 'chana',
+    'butternut squash' : 'squash',
+    'lettuce' : 'cabbage',
+    'broccoli' : 'cauliflower',
+    'blueberries' : 'pomegranate',
+    'watermelon' : 'mango',
+    'arugula' : 'spinach',
+    'olives' : 'mushrooms',
+    'zucchini' : 'squash',
+    'spaghetti squash' : 'squash',
+    'noodles' : 'rice',
+    'penne' : 'rice',
+    'pasta': 'rice',
+    'spaghetti' :'rice',
+    'macaroni' : 'rice',
+    'farfalle': 'rice',
+    'macaroni': 'rice',
+    'elbow pasta' : 'rice',
+    'pasta' :'rice',
+    'peanut oil' : 'vegetable oil',
+    'cornstarch' : 'maida',
+    'margarine' : 'ghee',
+    'butter':'ghee'
+}
+
+ 
+
 
 
 def recipe_transform(ingredients):
-    transformation = raw_input('Choose a transformation (h = healthy, v = vegetarian, gf = gluten free, asian = Asian): ')
+    transformation = raw_input('Choose a transformation (h = healthy, v = vegetarian, gf = gluten free, asian = Asian, italian = Italian, indian = Indian): ')
 
     if transformation == 'h':
         count = 0
@@ -87,6 +224,20 @@ def recipe_transform(ingredients):
         count = 0
         for i in ingredients:
             new_ing = asian.get(i,i)
+            ingredients[count] = new_ing
+            count += 1
+        print ingredients
+    elif transformation == 'italian':
+        count = 0
+        for i in ingredients:
+            new_ing = italian.get(i,i)
+            ingredients[count] = new_ing
+            count += 1
+        print ingredients
+    elif transformation == 'indian':
+        count = 0
+        for i in ingredients:
+            new_ing = indian.get(i,i)
             ingredients[count] = new_ing
             count += 1
         print ingredients
